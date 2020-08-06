15141a889fb8222f412070de874e8ca145ea15b9
macc2020 <makose00@gmail.com>
2020-08-06 04:01:27 +0300

    Second step

--- src/utility/GitFun.java
+++ src/utility/GitFun.java
@@ -3,5 +3,7 @@ package utility;
 public class GitFun {
     public static void main(String[] args) {
         System.out.println("Everything is fun");
+
+        System.out.println("something");
     }
 }
