109c7df6222effef71db2934c8e5db3c15cb3b10
macc2020 <makose00@gmail.com>
2020-08-06 04:43:45 +0300

    add Gitfun

--- src/utility/GitFun.java
+++ src/utility/GitFun.java
@@ -1,1 +1,11 @@
+package utility;
 
+public class GitFun {
+    public static void main(String[] args) {
+        System.out.println("Everything is fun");
+
+        System.out.println("something");
+
+        //make changes
+    }
+}
